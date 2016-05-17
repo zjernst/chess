9f17f519557a734fe8e102e31d3216b1796d5d85
App Academy Student <student@appacademy.io>
2016-05-17 16:38:14 -0700

    finalized setup

--- game.rb
+++ game.rb
@@ -17,7 +17,7 @@ class Chess
   }
 
   attr_reader :board
-  
+
   def initialize
     @board = setup
   end
@@ -26,19 +26,14 @@ class Chess
     board = Board.new
     pawn_setup(board)
     piece_setup(board)
-    # rook_setup(board)
-    # knight_setup(board)
-    # bishop_setup(board)
-    # queen_setup(board)
-    # king_setup(board)
+
     board
-    #everything else setup
+
   end
 
   def piece_setup(board)
     CLASSES.each do |piece, type|
-      positions = POSITIONS[piece]
-      positions.each do |position|
+      POSITIONS[piece].each do |position|
         color = position[0] == 0 ? :black : :white
         board[position] = type.new(color, position, board)
       end
@@ -52,54 +47,3 @@ class Chess
     end
     board
   end
-
-  def rook_setup(board)
-    positions = [[0,0], [0,7], [7,0],[7,7]]
-
-    positions.each do |position|
-      color = position[0] == 0 ? :black : :white
-      board[position] = Rook.new(color, position, board)
-    end
-    board
-  end
-
-  def knight_setup(board)
-    positions = [[0,1], [0,6], [7,1],[7,6]]
-
-    positions.each do |position|
-      color = position[0] == 0 ? :black : :white
-      board[position] = Knight.new(color, position, board)
-    end
-    board
-  end
-
-  def bishop_setup(board)
-    positions = [[0,2], [0,5], [7,2],[7,5]]
-
-    positions.each do |position|
-      color = position[0] == 0 ? :black : :white
-      board[position] = Bishop.new(color, position, board)
-    end
-    board
-  end
-
-  def queen_setup(board)
-    positions = [[0,4], [7,3]]
-
-    positions.each do |position|
-      color = position[0] == 0 ? :black : :white
-      board[position] = Queen.new(color, position, board)
-    end
-    board
-  end
-
-  def king_setup(board)
-    positions = [[0,3], [7,4]]
-
-    positions.each do |position|
-      color = position[0] == 0 ? :black : :white
-      board[position] = King.new(color, position, board)
-    end
-    board
-  end
-end
